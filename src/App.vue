6bde7e86c455968b52d892a647d74911413a5efc
Andrei Gerasimenko <gerasimenko_andrei@bk.ru>
2022-07-13 21:38:01 +0700

    new component for form

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <LoginForm />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import LoginForm from './components/LoginForm.vue';
 
 @Options({
   components: {
     HelloWorld,
+    LoginForm,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+body {
+    margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 100vw;
+    height: 100vh;
 }
 </style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="loginForm">
+    <div class="form"></div>
+    <div class="background">
+      <img
+        class="background-img"
+        src="https://img2.akspic.ru/previews/1/6/7/3/4/143761/143761-zakat-apelsin-voshod_solnca-gorizont-gornyj_relef-x750.jpg"
+        alt="background"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class LoginForm extends Vue {
+  msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.loginForm {
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  .form {
+    width: 55%;
+  }
+
+  .background {
+    width: 45%;
+    position: relative;
+    overflow: hidden;
+
+    .background-img {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
